4caef1bf48d1cdac4a1b939ed5ba32c8a71d6d39
gae4it <gae4it@gmail.com>
2025-07-19 11:59:49 +0200

    Fix index.html path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  root: "client", // Set the root to the client directory
   base: "/shoppinglistmanager/",
   plugins: [
     react(),
